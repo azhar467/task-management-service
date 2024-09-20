a77e22b4a824f03494a98dfb00d49bf1683b66f9
azhar467 <azhar.ahmed467@gmail.com>
2024-09-20 14:12:34 +0530

    skipping failed tests and correcting entity in repo

--- src/main/java/com/azhar/taskmanagement/repository/TaskRepository.java
+++ src/main/java/com/azhar/taskmanagement/repository/TaskRepository.java
@@ -1,8 +1,9 @@
 package com.azhar.taskmanagement.repository;
 
+import com.azhar.taskmanagement.dao.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends JpaRepository<TaskRepository,Long> {
+public interface TaskRepository extends JpaRepository<Task,Long> {
 }
