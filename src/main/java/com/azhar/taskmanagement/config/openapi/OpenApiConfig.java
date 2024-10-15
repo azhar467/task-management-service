fd3fa306ccd2656a9fbd2b9f6e923e50dfa177ad
azhar467 <azhar.ahmed467@gmail.com>
2024-10-15 14:20:36 +0530

    adding open api docementation

---
+++ src/main/java/com/azhar/taskmanagement/config/openapi/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.azhar.taskmanagement.config.openapi;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Task Management API",
+        version = "1.0",
+        description = "API for managing tasks, users, and projects.",
+            contact = @Contact(name = "Azhar Ahmed A",email = "azhar.ahmed467@gmail.com")
+    )
+)
+public class OpenApiConfig {
+    // You can add more custom configurations here if necessary
+}
--- src/main/java/com/azhar/taskmanagement/controller/UserController.java
+++ src/main/java/com/azhar/taskmanagement/controller/UserController.java
@@ -3,6 +3,10 @@ package com.azhar.taskmanagement.controller;
 import com.azhar.taskmanagement.service.BaseService;
 import com.azhar.taskmanagement.dao.dto.UserDTO;
 import com.azhar.taskmanagement.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,16 +26,27 @@ public class UserController extends BaseService {
     protected UserService userService;
 
     @PostMapping("/addUser")
-    public ResponseEntity<CompletableFuture<UserDTO>> saveUser(@ParameterObject @RequestBody UserDTO userDTO){
+    @Operation(summary = "Create a new user", description = "Creates a new user in the system.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created successfully."),
+            @ApiResponse(responseCode = "400", description = "Bad request - invalid input data.")})
+    public ResponseEntity<CompletableFuture<UserDTO>> saveUser(
+            @ParameterObject @RequestBody UserDTO userDTO) {
         log.info("Invoked on Thread: {}", Thread.currentThread().getName());
         userDTO.setRole(userDTO.getRole().toUpperCase());
         userDTO.setGender(userDTO.getGender().toUpperCase());
         return new ResponseEntity<>(userService.saveUser(userDTO), HttpStatus.CREATED);
     }
 
-    @GetMapping({"/{id}",""})
-    public ResponseEntity<CompletableFuture<List<UserDTO>>> getUsers(@PathVariable(required = false) Long id){
-        if (id!=null){
+    @GetMapping({"/{id}", ""})
+    @Operation(summary = "Get user(s)", description = "Retrieves a user by ID or all users if no ID is provided.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Users retrieved successfully."),
+            @ApiResponse(responseCode = "404", description = "User not found.")})
+    public ResponseEntity<CompletableFuture<List<UserDTO>>> getUsers(
+            @Parameter(description = "ID of the user to retrieve. If not provided, all users will be retrieved.")
+            @PathVariable(required = false) Long id) {
+        if (id != null) {
             return ResponseEntity.ok(userService.getUserById(id).thenApply(List::of));
         } else {
             return ResponseEntity.ok(userService.getAllUsers());
@@ -39,12 +54,27 @@ public class UserController extends BaseService {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<CompletableFuture<UserDTO>> updateUser(@PathVariable Long id, @ParameterObject @RequestBody UserDTO userDTO){
-        return ResponseEntity.ok(userService.updateUser(id,userDTO));
+    @Operation(summary = "Update an existing user", description = "Updates a user's details based on their ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User updated successfully."),
+            @ApiResponse(responseCode = "400", description = "Bad request - invalid input data."),
+            @ApiResponse(responseCode = "404", description = "User not found.")
+    })
+    public ResponseEntity<CompletableFuture<UserDTO>> updateUser(
+            @Parameter(description = "ID of the user to update.", required = true)
+            @PathVariable Long id, @ParameterObject @RequestBody UserDTO userDTO) {
+        return ResponseEntity.ok(userService.updateUser(id, userDTO));
     }
 
-    @DeleteMapping("/users/{id}")
-    public ResponseEntity<CompletableFuture<Void>> deleteUser(@PathVariable(value = "id") Long id){
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a user", description = "Deletes a user based on their ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "User deleted successfully."),
+            @ApiResponse(responseCode = "404", description = "User not found.")
+    })
+    public ResponseEntity<CompletableFuture<Void>> deleteUser(
+            @Parameter(description = "ID of the user to delete.", required = true)
+            @PathVariable(value = "id") Long id) {
         return ResponseEntity.ok(userService.deleteUser(id));
     }
 }
