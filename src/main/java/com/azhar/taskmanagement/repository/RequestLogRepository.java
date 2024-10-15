760db65ef4802c424b30507a80be6849c3f97133
azhar467 <azhar.ahmed467@gmail.com>
2024-10-15 13:06:43 +0530

    removed request logger

--- src/main/java/com/azhar/taskmanagement/repository/RequestLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.azhar.taskmanagement.repository;
-
-import com.azhar.taskmanagement.dao.RequestLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RequestLogRepository extends JpaRepository<RequestLog, Long> {
-}
